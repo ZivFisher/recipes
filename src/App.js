8434df42eb970c45210654ab4f05e9c5321418e4
ziv fisher <fisher.ziv@gmail.com>
2022-06-05 14:45:33 +0300

    commit on main

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="Appj">
+    <div className="App">
       <BrowserRouter>
         <Navbar />
         <Search />
